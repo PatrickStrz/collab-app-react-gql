99da645e47d202b68ccea787e5f6e24df3c18154
PatrickStrz <patrick.strzelec@gmail.com>
2017-05-04 17:26:50 -0400

    basic delete working

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,14 +1,18 @@
 import React,{Component} from 'react'
 // import propTypes from 'prop-types'
-// import { graphql } from 'react-apollo'
 import { graphql, compose } from 'react-apollo'
 import {getPostQuery} from '../queries/postQueries'
+import {deletePostMutation, updatePostMutation} from '../mutations/postMutations'
 // import {withRouter} from 'react-router-dom'
 import {Link} from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
 // import Paper from 'material-ui/Paper'
 
-
 class PostDetail extends Component {
+
+  handleDelete = async () => {
+      await this.props.deletePostMutation()
+  }
 
   render(){
 
@@ -19,27 +23,37 @@ class PostDetail extends Component {
     }
 
     const {id,description,imageUrl} = this.props.data.Post
+
     return(
   <div>
     <h1>Post detail for post:{id}</h1>
     <Link to='/'>go back home</Link>
+    <RaisedButton name="delete" secondary={true} onTouchTap={()=> this.handleDelete()}>Delete</RaisedButton>
     <p>
       {description}
     </p>
     <img src={imageUrl} alt="" style={{height:"500px", width:"500px"}}></img>
+
   </div>
+
     )
   }
 }
 
-const PostWithQuery = graphql(getPostQuery, {
-  options: ({match}) => (
-    {
+const options = ({match}) => ({
       variables:{
         id: match.params.id
       }
-    }
-  )
-})(PostDetail)
+    })
 
-export default PostWithQuery
+const PostWithDataAndMutations = compose(
+  graphql(deletePostMutation,{
+    options,
+    name: "deletePostMutation",
+  }),
+  graphql(getPostQuery,{
+    options
+  })
+)(PostDetail)
+
+export default PostWithDataAndMutations
