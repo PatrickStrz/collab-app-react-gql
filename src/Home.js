4a8286e8b7b220566fe7ca40a7526946648fecd5
PatrickStrz <patrick.strzelec@gmail.com>
2017-05-04 11:55:27 -0400

    added react router

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Posts from './Posts'
+import CreateChallenge from './CreateChallenge'
+const Home = () =>{
+  return(
+    <div>
+      <Posts />
+      <CreateChallenge />
+    </div>
+  )
+}
+
+export default Home
